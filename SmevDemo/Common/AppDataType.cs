beea53886a14009a01635001354adc49f89bab39
Koc <K.Subbotin@ics.perm.ru>
2015-10-19 21:58:26 +0500

    Забытые объекты.

---
+++ SmevDemo/Common/AppDataType.cs
@@ -0,0 +1,12 @@
+﻿namespace Common
+{
+    using System.Runtime.Serialization;
+    using System.Xml;
+
+    [DataContract]
+    public class AppDataType
+    {
+        [DataMember(Order = 0)]
+        public XmlElement[] Any { get; set; }
+    }
+}
